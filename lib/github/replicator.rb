97f22f0a2e5ccc9dbba583026cb6b595a605851e
Ryan Tomayko <ryan@github.com>
2011-09-03 00:54:36 -0700

    Loader is basically working

--- lib/github/replicator.rb
+++ lib/github/replicator.rb
@@ -1,6 +1,6 @@
 module GitHub
   module Replicator
-    # Dumps replicants in a streaming fashion.
+    # Dump replicants in a streaming fashion.
     #
     # The Dumper takes an ActiveRecord object and generates one or more replicant
     # objects. A replicant object has the form: [type, id, attributes] and
@@ -134,7 +134,87 @@ module GitHub
       end
     end
 
+    # Load replicants in a streaming fashion.
+    #
+    # The Loader reads [type, id, attributes] replicant tuples and creates
+    # objects in the current environment.
+    #
+    # Objects are expected to arrive in order such that a record referenced via
+    # foreign key always precedes the referencing record. The Loader maintains a
+    # mapping of primary keys from the dump system to the current environment.
+    # This mapping is used to properly establish new foreign key values on all
+    # records inserted.
     class Loader
+      def initialize
+        fail "not ready for production" if RAILS_ENV == 'production'
+        @keymap = {}
+      end
+
+      def load(type, id, attributes)
+        model = Object::const_get(type)
+        instance = load_object model, attributes
+        primary_key = nil
+
+        attributes.each do |key, value|
+          if key == model.primary_key
+            primary_key = value
+            next
+          elsif key =~ /^(.*)_id$/
+            if value && reflection = model.reflect_on_association($1.to_sym)
+              dependent_model = Object::const_get(reflection.class_name)
+              if record = find_dependent_object(dependent_model, value)
+                instance.write_attribute key, record.id
+              else
+                warn "error: #{model} referencing #{dependent_model}:#{value} not found in keymap"
+              end
+            elsif value
+              warn "warn: association not found for #{model}.#{key} attribute"
+              instance.write_attribute key, value
+            end
+          else
+            instance.write_attribute key, value
+          end
+        end
+
+        instance.save false
+
+        # register in keymap for type subclass and any superclasses
+        while model != ActiveRecord::Base && model != Object
+          @keymap["#{model}:#{primary_key}"] = instance
+          model = model.superclass
+        end
+
+        instance
+      end
+
+      def load_object(model, attributes)
+        meth = "load_#{model.to_s.underscore}"
+        instance =
+          if respond_to?(meth)
+            send(meth, attributes) || model.new
+          else
+            model.new
+          end
+        def instance.callback(*args);end # Rails 2.x hack to disable callbacks.
+        instance
+      end
+
+      def find_dependent_object(model, id)
+        @keymap["#{model}:#{id}"]
+      end
+
+      def load_user(attrs)
+        User.find_by_login(attrs['login'])
+      end
+
+      def load_repository(attrs)
+        owner = find_dependent_object(User, attrs['owner_id'])
+        if repo = Repository.find_by_name_with_owner("#{owner.login}/#{attrs['name']}")
+          warn "warn: deleting existing repository: #{repo.name_with_owner} (#{repo.id})"
+          repo.destroy
+        end
+        Repository.new
+      end
     end
   end
 end
