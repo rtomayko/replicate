60d5380e1c3c48ffb18571e81b48813a05c4aa23
Ryan Tomayko <rtomayko@gmail.com>
2011-09-09 08:07:26 -0700

    replicate command requires --force to load in production environment

--- lib/replicate.rb
+++ lib/replicate.rb
@@ -6,5 +6,19 @@ module Replicate
   autoload :Status,  'replicate/status'
   autoload :AR,      'replicate/active_record'
 
+  # Determine if this is a production looking environment. Used in bin/replicate
+  # to safeguard against loading in production.
+  def self.production_environment?
+    if defined?(Rails) && Rails.respond_to?(:env)
+      Rails.env.to_s = 'production'
+    elsif defined?(RAILS_ENV)
+      RAILS_ENV == 'production'
+    elsif ENV['RAILS_ENV']
+      ENV['RAILS_ENV'] == 'production'
+    elsif ENV['RACK_ENV']
+      ENV['RAILS_ENV'] == 'production'
+    end
+  end
+
   AR if defined?(::ActiveRecord::Base)
 end
